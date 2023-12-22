5514a2f82b8088c9bde0389a53f69a638ce5e55a
Sergey Ladanov <ladanov@digitrans.ru>
2023-12-22 16:00:53 +0300

    add

--- Components/ComponentBase.py
+++ Components/ComponentBase.py
@@ -1,17 +1,21 @@
 
 import re
 
-TYPE_NONE = 0
-TYPE_SMRES = 1
-TYPE_AXIALRES = 2
-TYPE_SMCCAP = 3
-TYPE_AXIALCCAP = 4
-TYPE_SMTCAP = 5
-TYPE_AXIALTCAP = 6
-TYPE_SMIND = 7
-TYPE_AXIALIND = 8
+
 
 class ComponentBase:
+
+    TYPE_NONE = 0
+    TYPE_SMRES = 1
+    TYPE_AXIALRES = 2
+    TYPE_SMCCAP = 3
+    TYPE_AXIALCCAP = 4
+    TYPE_SMTCAP = 5
+    TYPE_AXIALTCAP = 6
+    TYPE_SMIND = 7
+    TYPE_AXIALIND = 8
+
+
     def __init__(self, name=""):
         self.__Name = name
         self.__Designator = ""
@@ -21,13 +25,15 @@ class ComponentBase:
         self.__Case = ""
         self.__Endurance = 0.0
         self.__UnitsEndurance = ""
-        self.__Type = TYPE_NONE
+        self.__Type = self.TYPE_NONE
 
         self.__Parse(self.__Name)
 
-    def __Parse(self, name):
 
-        # Try to get power value and units in English
+
+
+    def __ParseEndurance(self, name):
+                # Try to get power value and units in English
         res = re.search(r'[+-]?([0-9]*[.])?[0-9]+[\s]?\w?W', name)
         if res:
             self.__Endurance = float(re.search(r'[+-]?([0-9]*[.])?[0-9]+', res[0])[0])
@@ -40,8 +46,34 @@ class ComponentBase:
                 self.__UnitsEndurance = re.search(r'\D?Вт', res[0])[0]
 
 
+        # Try to get voltage value and units in English
+        res = re.search(r'[+-]?([0-9]*[.])?[0-9]+[\s]?\w?V', name)
+        if res:
+            self.__Endurance = float(re.search(r'[+-]?([0-9]*[.])?[0-9]+', res[0])[0])
+            self.__UnitsEndurance = re.search(r'\D?V', res[0])[0]
+        else:
+            # Try to get voltage value and units in Russian
+            res = re.search(r'[+-]?([0-9]*[.])?[0-9]+[\s]?\w?В\W', name)
+            if res:
+                self.__Endurance = float(re.search(r'[+-]?([0-9]*[.])?[0-9]+', res[0])[0])
+                self.__UnitsEndurance = re.search(r'\D?В', res[0])[0]
 
-        
+
+        # Try to get current value and units in English
+        res = re.search(r'[+-]?([0-9]*[.])?[0-9]+[\s]?\w?A', name)
+        if res:
+            self.__Endurance = float(re.search(r'[+-]?([0-9]*[.])?[0-9]+', res[0])[0])
+            self.__UnitsEndurance = re.search(r'\D?A', res[0])[0]
+        else:
+            # Try to get current value and units in Russian
+            res = re.search(r'[+-]?([0-9]*[.])?[0-9]+[\s]?\w?А\W', name)
+            if res:
+                self.__Endurance = float(re.search(r'[+-]?([0-9]*[.])?[0-9]+', res[0])[0])
+                self.__UnitsEndurance = re.search(r'\D?А', res[0])[0]
+
+
+
+    def __ParseUnits(self, name):
         # Try to get res value and units value in Russian
         res = re.search(r'[+-]?([0-9]*[.])?[0-9]+[\s]?\w?Ом', name)
         if res:
@@ -61,6 +93,33 @@ class ComponentBase:
 
                 if temp_units:
                     self.__UnitsValue = temp_units + self.__UnitsValue
+
+
+
+    def __ParseTolerance(self, name):
+        # Try to get tolerance
+        res = re.search(r'[+-]?([0-9]*[.])?[0-9]+\s?%', name)
+
+        if res:
+            self.__Tolerance = float(re.search(r'[+-]?([0-9]*[.])?[0-9]+', res[0])[0])
+
+
+    def __ParseCase(self, name):
+        # Try to get case
+        res = re.search(r'\s?[0-9][0-9][0-9][0-9]\s?', name)
+
+        if res:
+            self.__Case = res[0]
+
+
+
+
+
+    def __Parse(self, name):
+        self.__ParseEndurance(name)
+        self.__ParseUnits(name)
+        self.__ParseTolerance(name)
+        self.__ParseCase(name)
 
 
 

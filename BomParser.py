b4f2946b23085591a7f9fdcef0d002f6647181c4
Sergey Ladanov <ladanov@digitrans.ru>
2023-12-22 15:00:15 +0300

    add

--- BomParser.py
+++ BomParser.py
@@ -1,29 +1,35 @@
 import sys, os
 sys.path.append(os.path.abspath(os.path.join('Components')))
 
-import ComponentBase
+import ComponentBase as Component
 
 import re
 
 
+test = Component.ComponentBase("5.1k 1% 0.063W 0603")
+
+test = Component.ComponentBase("5.1 кОм 1% 0.063Вт 0603")
+
+print("Test")
+
 # test_var = re.search("", "test_123")
 
 
-test_var = re.search(r'[+-]?([0-9]*[.])?[0-9]+[\s]?\w?Wt', "test_123.6 kWt")
+# test_var = re.search(r'[+-]?([0-9]*[.])?[0-9]+[\s]?\w?Wt', "test_123.6 kWt")
 
-print(test_var[0] if test_var else 'Not found')
-
-
-test_var = re.search(r'[+-]?([0-9]*[.])?[0-9]+[\s]?\w?Wt', "test_123.6 Wt")
-
-print(test_var[0] if test_var else 'Not found')
+# print(test_var[0] if test_var else 'Not found')
 
 
-test_var = re.search(r'[+-]?([0-9]*[.])?[0-9]+[\s]?\w?Вт', "test_123.6 кВт")
+# test_var = re.search(r'[+-]?([0-9]*[.])?[0-9]+[\s]?\w?Wt', "test_123.6 Wt")
 
-print(test_var[0] if test_var else 'Not found')
+# print(test_var[0] if test_var else 'Not found')
 
 
-test_var = re.search(r'[+-]?([0-9]*[.])?[0-9]+[\s]?\w?Вт', "test_123.6 Вт")
+# test_var = re.search(r'[+-]?([0-9]*[.])?[0-9]+[\s]?\w?Вт', "test_123.6 кВт")
 
-print(test_var[0] if test_var else 'Not found')
+# print(test_var[0] if test_var else 'Not found')
+
+
+# test_var = re.search(r'[+-]?([0-9]*[.])?[0-9]+[\s]?\w?Вт', "test_0.063 Вт")
+
+# print(test_var[0] if test_var else 'Not found')
--- Components/ComponentBase.py
+++ Components/ComponentBase.py
@@ -1,9 +1,15 @@
 
+import re
+
 TYPE_NONE = 0
 TYPE_SMRES = 1
-TYPE_SMCCAP = 2
-TYPE_SMTCAP = 3
-TYPE_SMIND = 4
+TYPE_AXIALRES = 2
+TYPE_SMCCAP = 3
+TYPE_AXIALCCAP = 4
+TYPE_SMTCAP = 5
+TYPE_AXIALTCAP = 6
+TYPE_SMIND = 7
+TYPE_AXIALIND = 8
 
 class ComponentBase:
     def __init__(self, name=""):
@@ -17,8 +23,48 @@ class ComponentBase:
         self.__UnitsEndurance = ""
         self.__Type = TYPE_NONE
 
+        self.__Parse(self.__Name)
+
     def __Parse(self, name):
-        pass
+
+        # Try to get power value and units in English
+        res = re.search(r'[+-]?([0-9]*[.])?[0-9]+[\s]?\w?W', name)
+        if res:
+            self.__Endurance = float(re.search(r'[+-]?([0-9]*[.])?[0-9]+', res[0])[0])
+            self.__UnitsEndurance = re.search(r'\D?W', res[0])[0]
+        else:
+            # Try to get power value and units in Russian
+            res = re.search(r'[+-]?([0-9]*[.])?[0-9]+[\s]?\w?Вт', name)
+            if res:
+                self.__Endurance = float(re.search(r'[+-]?([0-9]*[.])?[0-9]+', res[0])[0])
+                self.__UnitsEndurance = re.search(r'\D?Вт', res[0])[0]
+
+
+
+        
+        # Try to get res value and units value in Russian
+        res = re.search(r'[+-]?([0-9]*[.])?[0-9]+[\s]?\w?Ом', name)
+        if res:
+            self.__Value = float(re.search(r'[+-]?([0-9]*[.])?[0-9]+', res[0])[0])
+            self.__UnitsValue = re.search(r'\D?Ом', res[0])[0]
+                
+        else:
+            # Try to get res value and units in English
+            res = re.search(r'[+-]?([0-9]*[.])?[0-9]+[\s]?[kKmM]?\W?', name)
+            if res:
+
+                self.__Value = float(re.search(r'[+-]?([0-9]*[.])?[0-9]+', res[0])[0])
+
+                temp_units = re.search(r'\s?[^0-9.]', res[0])[0]
+
+                self.__UnitsValue = "Ohm"
+
+                if temp_units:
+                    self.__UnitsValue = temp_units + self.__UnitsValue
+
+
+
+
 
 
     def GetOrderName(self):

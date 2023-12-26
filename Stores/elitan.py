a8ef22e490a374b75547e9881e0f59dce9395a43
Sergey Ladanov <ladanov@digitrans.ru>
2023-12-26 10:00:50 +0300

    add resistor name generator

--- Stores/elitan.py
+++ Stores/elitan.py
@@ -14,14 +14,31 @@ def __count_decimal_places(number):
 
 def __GenerateValueForResistor(component_obj):
     res = ""
+    units_str = component_obj.GetUnitsValue()
+
+    probe = re.search(r'[а-яА-Я]?Ом', units_str)
+
+    if probe:
+        units_str = re.sub(r'[Ооo][hH]?[мМm]', 'Ohm', units_str)
+        units_str = re.sub(r'[кK]', 'k', units_str)
+        units_str = re.sub(r'[М]', 'M', units_str)
 
 
-    probe = re.search(r'[a-zA-Z]?Ohm', component_obj.GetUnitsValue())
+
+
+
+
+    probe = re.search(r'[a-zA-Z]?Ohm', units_str)
     if  probe:
         probe = probe[0].replace('Ohm', '')
         if probe != "":
             number_str = str(component_obj.GetValue())
             res = number_str.replace('.', probe.upper())
+
+            probe = re.search(r'[A-Z][0]*[1-9]', res)
+
+            if not probe:
+                res = res[:-1]
         else:
             res = str(component_obj.GetValue()) + 'R'
 
@@ -29,7 +46,22 @@ def __GenerateValueForResistor(component_obj):
 
         if (probe):
             res = res.replace('.0', '')
+    return res
 
+
+def __GenerateToleranceForResistor(component_obj):
+    res = "*"
+    if component_obj.GetTolerance() == 0.1:
+        res = "B"
+
+    if component_obj.GetTolerance() == 0.5:
+        res = "D"
+
+    if component_obj.GetTolerance() == 1.0:
+        res = "F"
+
+    if component_obj.GetTolerance() == 5.0:
+        res = "J"
 
     return res
 
@@ -43,8 +75,11 @@ def __GenerateValueForCapacitor(component_obj):
 def GenerateFindRequest(component_obj):
     res = ""
 
-    if component_obj.GetMountWay() == component_obj.MOUNT_WAY_SMD:
-        if component_obj.GetDesignator() == "R":
-           res = f'SMRES/{component_obj.GetCase():s}-{__GenerateValueForResistor(component_obj):s}-F'
+    if component_obj.GetManufacturerPartNumber() != "":
+        res = component_obj.GetManufacturerPartNumber()
+    else:
+        if component_obj.GetMountWay() == component_obj.MOUNT_WAY_SMD:
+            if component_obj.GetDesignator() == "R":
+                res = f'SMRES/{component_obj.GetCase():s}-{__GenerateValueForResistor(component_obj):s}-{__GenerateToleranceForResistor(component_obj):s}'
 
     return res

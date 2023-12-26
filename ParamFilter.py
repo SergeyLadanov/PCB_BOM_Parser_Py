29ff0b8bbd042a83f5517e7cc9bf55397d1d5048
Sergey Ladanov <ladanov@digitrans.ru>
2023-12-26 16:27:30 +0300

    developing param filters

--- ParamFilter.py
+++ ParamFilter.py
@@ -1,2 +1,19 @@
 
+class FilterItem:
+    def __init__(self, skip_endurance = False, skip_tolerance = False, skip_designvariant = False):
+        self.SkipEndurance = skip_endurance
+        self.SkipTolerance = skip_tolerance
+        self.SkipVariant = skip_designvariant
 
+
+class FilterObj:
+    FilterData = {
+                  'C': FilterItem(),
+                  'R': FilterItem(),
+                  'L': FilterItem(),
+                  }
+    def SetFilter(self, key_str, filter):
+        self.FilterData[key_str] = filter
+
+    def SetSkipingEndurance(self, key_str, value):
+        self.FilterData[key_str].SkipEndurance = value

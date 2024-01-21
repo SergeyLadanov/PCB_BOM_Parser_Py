59fc2552f97aac06ab0c2ec4ca57a7a421f2586a
Sergey Ladanov <sergei_ladanov_93@mail.ru>
2024-01-21 17:41:38 +0300

    add

--- static/app/js/control.js
+++ static/app/js/control.js
@@ -1,6 +1,27 @@
 
 var ResTableRowCount = 1;
 
+const exampleModal = document.getElementById('exampleModal');
+if (exampleModal) {
+  exampleModal.addEventListener('show.bs.modal', event => {
+    // Button that triggered the modal
+    const button = event.relatedTarget
+    // Extract info from data-bs-* attributes
+    const recipient = button.getAttribute('data-bs-whatever')
+    // If necessary, you could initiate an Ajax request here
+    // and then do the updating in a callback.
+
+    // Update the modal's content.
+    const modalTitle = exampleModal.querySelector('.modal-title')
+    const modalBodyInput = exampleModal.querySelector('.modal-body input')
+
+    modalTitle.textContent = `Список для заказа ${recipient}`
+    modalBodyInput.value = recipient
+  })
+}
+
+
+
 function ClearResTable()
 {
     ResTableRowCount = 1;

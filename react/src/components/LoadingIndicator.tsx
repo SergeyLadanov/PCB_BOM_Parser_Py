94a9d0aa7b5153ad0c875af61cc5bac94db22ec3
Sergey Ladanov <sergei_ladanov_93@mail.ru>
2024-10-24 13:03:32 +0300

    add

--- react/src/components/LoadingIndicator.tsx
+++ react/src/components/LoadingIndicator.tsx
@@ -4,14 +4,11 @@ import '../scss/styles.scss'
 function LoadingIndicator() {
   return (
     <div className="row" style={{ textAlign: 'center' }} id="loading">
-      <p></p>
-      <p>
         <div className="col-md-12">
           <div className="spinner-border" role="status">
             <span className="visually-hidden">Loading...</span>
           </div>
         </div>
-      </p>
     </div>
   )
 }

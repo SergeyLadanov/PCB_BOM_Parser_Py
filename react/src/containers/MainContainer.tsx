36bd3123c49d5dcf3f522d6c3bccc7e826bef186
Sergey Ladanov <sergei_ladanov_93@mail.ru>
2024-10-28 23:28:56 +0300

    add

--- react/src/containers/MainContainer.tsx
+++ react/src/containers/MainContainer.tsx
@@ -88,7 +88,6 @@ function MainContainer() {
     return () => {}
   })
 
-
   const replaceAll = (str: string, find: string, replace: string) => {
     return str.replace(new RegExp(find, 'g'), replace)
   }
